cddf228a26888da4e67cf87fb513bcab0a3485ba
loftum <loftum@gmail.com>
2017-08-24 09:31:26 +0200

    Microsoft.Build packages

--- src/SolutionWatcher/Program.cs
+++ src/SolutionWatcher/Program.cs
@@ -2,8 +2,10 @@
 using System.CodeDom;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.MSBuild;
 
 namespace SolutionWatcher
@@ -12,7 +14,7 @@ namespace SolutionWatcher
     {
         static int Main(string[] args)
         {
-            Console.WriteLine("Solution watcher started");
+            Console.WriteLine("Solution watcher v0.0");
             if (!args.Any())
             {
                 PrintUsage();
@@ -25,15 +27,40 @@ namespace SolutionWatcher
                     Console.CancelKeyPress += (o, e) =>
                     {
                         source.Cancel();
+                    };
+                    try
+                    {
+                        Run(args, source.Token).Wait(source.Token);
+                        Console.WriteLine("Bye!");
+                    }
+                    catch (OperationCanceledException e)
+                    {
                         Console.ForegroundColor = ConsoleColor.Cyan;
                         Console.WriteLine("CANCELLED");
                         Console.ResetColor();
-                    };
-                    Run(args, source.Token).Wait(source.Token);
-                    
-                    Console.WriteLine("Bye!");
-                    return 0;
+                    }
+                    catch (AggregateException a)
+                    {
+                        var inner = a.InnerException ?? a.InnerExceptions.FirstOrDefault();
+                        if (inner != null)
+                        {
+                            throw inner;
+                        }
+                        throw;
+                    }
                 }
+            }
+            catch (ReflectionTypeLoadException e)
+            {
+                Console.WriteLine("Dang!");
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Red;
+                foreach (var exception in e.LoaderExceptions)
+                {
+                    Console.WriteLine(exception);
+                    Console.WriteLine();
+                }
+                return -1;
             }
             catch (Exception e)
             {
@@ -43,7 +70,7 @@ namespace SolutionWatcher
                 Console.WriteLine(e.GetBaseException());
                 return -1;
             }
-
+            return 0;
         }
 
         private static void PrintUsage()
@@ -55,9 +82,35 @@ namespace SolutionWatcher
         {
             var sln = args[0];
             Console.WriteLine($"Opening {sln}");
-            var workspace = MSBuildWorkspace.Create();
-            //var solution = await workspace.OpenSolutionAsync(args[0], cancellationToken);
-            //Console.WriteLine($"Opened {solution.FilePath}");
+            using (var workspace = MSBuildWorkspace.Create())
+            {
+                workspace.WorkspaceChanged += WorkspaceChanged;
+                workspace.DocumentOpened += DocumentOpened;
+                workspace.DocumentClosed += DocumentClosed;
+                
+                var solution = await workspace.OpenSolutionAsync(args[0], cancellationToken);
+
+                Console.WriteLine($"Opened {solution.FilePath}");
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    await Task.Delay(500, cancellationToken);
+                }
+            }
+        }
+
+        private static void DocumentClosed(object sender, DocumentEventArgs e)
+        {
+            Console.WriteLine($"Document closed: {e.Document.Name}");
+        }
+
+        private static void DocumentOpened(object sender, DocumentEventArgs e)
+        {
+            Console.WriteLine($"Document opened: {e.Document.Name}");
+        }
+
+        private static void WorkspaceChanged(object sender, WorkspaceChangeEventArgs e)
+        {
+            Console.WriteLine($"Workspace changed: {e.Kind} {e.DocumentId}");
         }
     }
 }
