6d6f4a37abc7f91b1c1b38bf270f13a9251ee15a
loftum <loftum@gmail.com>
2019-05-24 15:45:47 +0200

    Data binding

--- netcore/Visualizer.Mac/ViewController.cs
+++ netcore/Visualizer.Mac/ViewController.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using AppKit;
-using CoreGraphics;
-using CoreText;
 using Foundation;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -15,6 +13,34 @@ using RoslynDemo.Core.Serializers;
 
 namespace Visualizer.Mac
 {
+    public class Locked<T>
+    {
+        private readonly object _lock = new object();
+        private T _value;
+
+        public T Get()
+        {
+            lock(_lock)
+            {
+                return _value;
+            }
+        }
+
+        public void Set(T value)
+        {
+            lock(_lock)
+            {
+                _value = value;
+            }
+        }
+
+        public static implicit operator T (Locked<T> locked)
+        {
+            return locked.Get();
+        }
+
+    }
+
     public partial class ViewController : NSViewController
     {
         private string _syntaxTreeText;
@@ -30,8 +56,28 @@ namespace Visualizer.Mac
             }
         }
 
-        private DateTimeOffset _parseAt;
-        private bool _doParse;
+        private readonly Locked<string> _inputText = new Locked<string>();
+        public string InputText
+        {
+            get => _inputText.Get();
+            set => _inputText.Set(value);
+        }
+
+        private readonly Locked<bool> _shouldParse = new Locked<bool>();
+        public bool ShouldParse
+        {
+            get => _shouldParse.Get();
+            set => _shouldParse.Set(value);
+        }
+
+
+        private readonly Locked<DateTimeOffset> _parseAt = new Locked<DateTimeOffset>();
+        public DateTimeOffset ParseAt
+        {
+            get => _parseAt.Get();
+            set => _parseAt.Set(value);
+        }
+
         private readonly Task _parser;
 
         private static readonly IEnumerable<PortableExecutableReference> References = Assemblies.FromCurrentContext()
@@ -52,46 +98,39 @@ namespace Visualizer.Mac
         {
             base.ViewDidLoad();
             _parser.Start();
-            SyntaxTreeBox.TextStorage.SetString(new NSAttributedString("Hello", StringAttributes));
             InputBox.TextDidChange += Parse;
             // Do any additional setup after loading the view.
         }
-
-        private static readonly CTStringAttributes StringAttributes = new CTStringAttributes
-        {
-            ForegroundColor = new CGColor(1.0f, 1.0f, 1.0f)
-        };
 
         private void DoParse()
         {
             while (true)
             {
-                if (!_doParse)
+                if (!ShouldParse)
                 {
-                    Thread.Sleep(500);
+                    Thread.Sleep(100);
                     continue;
                 }
-                _doParse = false;
-                while (_parseAt > DateTimeOffset.UtcNow)
+                ShouldParse = false;
+                while (DateTimeOffset.UtcNow < ParseAt)
                 {
-                    Thread.Sleep(500);
+                    Thread.Sleep(100);
                 }
 
-                Console.WriteLine("hello 1");
-
-                SyntaxTree = CSharpSyntaxTree.ParseText(InputBox.Value);
-                Console.WriteLine("hello 2");
+                var input = InputText;
+                Console.WriteLine($"Parsing {input}");
+                SyntaxTree = CSharpSyntaxTree.ParseText(input);
                 Compilation = CSharpCompilation.Create("hest", new[] { SyntaxTree }, References, new CSharpCompilationOptions(OutputKind.ConsoleApplication));
-                Console.WriteLine("hello 3");
                 var treeModel = SyntaxMapper.Map(SyntaxTree);
-                SyntaxTreeText = treeModel.ToJson(true, true);
+                InvokeOnMainThread(() => SyntaxTreeText = treeModel.ToJson(true, true));
             }
         }
 
         private void Parse(object sender, EventArgs e)
         {
-            _doParse = true;
-            _parseAt = DateTimeOffset.UtcNow.AddSeconds(1);
+            InputText = InputBox.Value;
+            ShouldParse = true;
+            ParseAt = DateTimeOffset.UtcNow.AddMilliseconds(300);
         }
 
         public override NSObject RepresentedObject
