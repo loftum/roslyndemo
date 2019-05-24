c650d439665409ce897bb4e94a22237672687cf3
loftum <loftum@gmail.com>
2019-05-24 21:51:48 +0200

    VisualizerModel

---
+++ netcore/RoslynDemo.Core/Models/VisualizerModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using RoslynDemo.Core.Models.Semantics;
+using RoslynDemo.Core.Syntax;
+
+namespace RoslynDemo.Core.Models
+{
+    public class VisualizerModel
+    {
+        private static readonly IEnumerable<PortableExecutableReference> References = Assemblies.FromCurrentContext()
+            .Append(typeof(Console).Assembly)
+            .Append(typeof(object).Assembly)
+            .Select(a => MetadataReference.CreateFromFile(a.Location));
+
+        public SyntaxTree SyntaxTree { get; }
+        public CSharpCompilation Compilation { get; }
+
+        public VisualizerModel(string input)
+        {
+            SyntaxTree = CSharpSyntaxTree.ParseText(input, CSharpParseOptions.Default);
+            Compilation = CSharpCompilation.Create("hest", new[] { SyntaxTree }, References, new CSharpCompilationOptions(OutputKind.ConsoleApplication));
+        }
+
+        public SyntaxMeta GetMetaAt(int index)
+        {
+            if (!SyntaxTree.TryGetRoot(out var root))
+            {
+                return SyntaxMeta.Empty;
+            }
+            var nodeOrToken = root.GetMostSpecificNodeOrTokenAt(index);
+            var semantics = GetSemantics(nodeOrToken, index);
+            return new SyntaxMeta(nodeOrToken, semantics);
+        }
+
+        private Dictionary<string, object> GetSemantics(SyntaxNodeOrToken nodeOrToken, int index)
+        {
+            var semantics = Compilation.GetSemanticModel(SyntaxTree);
+
+            var result = new Dictionary<string, object>();
+            var node = nodeOrToken.AsNode();
+            if (node != null)
+            {
+                result["TypeInfo"] = SymbolMapper.Map(semantics.GetTypeInfo(node));
+                result["SymbolInfo"] = SymbolMapper.Map(semantics.GetSymbolInfo(node));
+            }
+            result["EnclosingSymbol"] = SymbolMapper.Map(semantics.GetEnclosingSymbol(index));
+
+            return result;
+        }
+
+        public static VisualizerModel Parse(string input) => new VisualizerModel(input);
+    }
+}
--- netcore/Visualizer/ViewModels/MainViewModel.cs
+++ netcore/Visualizer/ViewModels/MainViewModel.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.CodeAnalysis;
+﻿using System.Collections.Generic;
 using Microsoft.CodeAnalysis.CSharp;
-using RoslynDemo.Core;
 using RoslynDemo.Core.IO;
 using RoslynDemo.Core.Models;
 using RoslynDemo.Core.Models.Emit;
-using RoslynDemo.Core.Models.Semantics;
 using RoslynDemo.Core.Models.Syntax;
-using RoslynDemo.Core.Syntax;
 using Visualizer.Completion;
 using Visualizer.Wpf;
 
@@ -19,68 +13,36 @@ namespace Visualizer.ViewModels
     {
         private readonly FileManager _fileManager = new FileManager("out");
 
-        public SyntaxTree SyntaxTree { get; private set; }
-        public CSharpCompilation Compilation { get; private set; }
+        private VisualizerModel _model;
 
         public void Parse(string input)
         {
-            SyntaxTree = CSharpSyntaxTree.ParseText(input, CSharpParseOptions.Default);
-            Compilation = CSharpCompilation.Create("hest", new[] { SyntaxTree }, References, new CSharpCompilationOptions(OutputKind.ConsoleApplication));
+            _model = VisualizerModel.Parse(input);
         }
 
         public SyntaxTreeModel GetTreeModel()
         {
-            return SyntaxMapper.Map(SyntaxTree);
+            return SyntaxMapper.Map(_model.SyntaxTree);
         }
-
-        private static readonly IEnumerable<PortableExecutableReference> References = Assemblies.FromCurrentContext()
-            .Append(typeof(Console).Assembly)
-            .Append(typeof(object).Assembly)
-            .Select(a => MetadataReference.CreateFromFile(a.Location));
 
         public CSharpCompilationModel GetCompilationModel()
         {
-            return CompilationMapper.Map(Compilation);
+            return CompilationMapper.Map(_model.Compilation);
         }
 
         public EmitResultModel Emit()
         {
             _fileManager.CleanFolder();
-            var result = Compilation.Emit(_fileManager.ToFullPath("program.dll"), _fileManager.ToFullPath("program.pdb"));
+            var result = _model.Compilation.Emit(_fileManager.ToFullPath("program.dll"), _fileManager.ToFullPath("program.pdb"));
             _fileManager.SaveJson(RuntimeConfig.Generate(), "program.runtimeconfig.json");
             return CompilationMapper.Map(result);
         }
 
-        public SyntaxMeta GetMetaAt(int index)
-        {
-            if (!SyntaxTree.TryGetRoot(out var root))
-            {
-                return SyntaxMeta.Empty;
-            }
-            var nodeOrToken = root.GetMostSpecificNodeOrTokenAt(index);
-            var semantics = GetSemantics(nodeOrToken, index);
-            return new SyntaxMeta(nodeOrToken, semantics);
-        }
-
-        private Dictionary<string, object> GetSemantics(SyntaxNodeOrToken nodeOrToken, int index)
-        {
-            var semantics = Compilation.GetSemanticModel(SyntaxTree);
-
-            var result = new Dictionary<string, object>();
-            var node = nodeOrToken.AsNode();
-            if (node != null)
-            {
-                result["TypeInfo"] = SymbolMapper.Map(semantics.GetTypeInfo(node));
-                result["SymbolInfo"] = SymbolMapper.Map(semantics.GetSymbolInfo(node));
-            }
-            result["EnclosingSymbol"] = SymbolMapper.Map(semantics.GetEnclosingSymbol(index));
-
-            return result;
-        }
+        public SyntaxMeta GetMetaAt(int index) => _model.GetMetaAt(index);
 
         public IEnumerable<CompletionData> GetCompletions(int location)
         {
-            var completer = new CodeCompleter(SyntaxTree, Compilation, location);
+            var completer = new CodeCompleter(_model.SyntaxTree, _model.Compilation, location);
             return completer.GetCompletions();
         }
     }
