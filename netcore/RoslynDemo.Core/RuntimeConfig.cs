c9cfe55c4d69edf6bcf34905ba9bc08910839490
loftum <loftum@gmail.com>
2019-05-25 20:02:21 +0200

    RuntimeConfig

---
+++ netcore/RoslynDemo.Core/RuntimeConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace RoslynDemo.Core
+{
+    public static class RuntimeConfig
+    {
+        public static Dictionary<string, object> Generate()
+        {
+            var version = "2.2.0";// Environment.Version;
+            //var version = Environment.Version;
+
+            return new Dictionary<string, object>
+            {
+                ["runtimeOptions"] = new Dictionary<string, object>
+                {
+                    ["tfm"] = $"netcoreapp{version}",
+                    ["framework"] = new Dictionary<string, object>
+                    {
+                        ["name"] = "Microsoft.NETCore.App",
+                        ["version"] = $"{version}"
+                    }
+                }
+            };
+        }
+    }
+}
