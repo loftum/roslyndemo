e51cf977629d2195c20636c19dd50e2d237e601d
loftum <loftum@gmail.com>
2019-05-26 16:15:48 +0200

    InitialTexts

---
+++ netcore/RoslynDemo.Core/InitialTexts.cs
@@ -0,0 +1,34 @@
+﻿namespace RoslynDemo.Core
+{
+    public static class InitialTexts
+    {
+        public const string HelloWorld = @"
+using System;
+
+public class Hest
+{
+  public event EventHandler OnKnegg;
+  public string Navn { get; set; }
+
+  public void Knegg()
+  {
+    Console.WriteLine(""Vrinsk!"");
+    OnKnegg?.Invoke(this, EventArgs.Empty);
+  }
+  
+  public static void Main(string[] kneggs)
+  {
+    var hest = new Hest { Navn = ""Klaus Knegg"", OnKnegg = HarKnegga};
+    hest.Knegg();
+    hest.OnKnegg -= HarKnegga;
+  }
+
+  private static void HarKnegga(object sender, EventArgs e)
+  {
+    var hest = (Hest) sender;
+    Console.WriteLine($""{hest.Navn} har knegga"");
+  }
+}
+";
+    }
+}
